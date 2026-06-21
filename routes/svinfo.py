ad683fe19f13fb26d025b1cf20a3943d06fa633b
TetoDev72 <winpocostudios@gmail.com>
2026-06-21 15:18:43 +0300

    update to support HEAD

--- routes/svinfo.py
+++ routes/svinfo.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import PlainTextResponse
 from data.config import API_VERSION, PROJECT_NAME, FLAVOR, MIRRORS, REPO_URL
 
 router = APIRouter()
 
-@router.get("/v")
-@router.get("/svinfo")
-async def server_info():
+@router.route("/v", methods=["GET", "HEAD"])
+@router.route("/svinfo", methods=["GET", "HEAD"])
+async def server_info(request: Request):
     mirrors_text = "\n".join([f"mirror: {m}" for m in MIRRORS]) if MIRRORS else "mirror: none"
     text = f"""{PROJECT_NAME}
 version: {API_VERSION}
